7fb4b44639eb48a6e1da00bc170913580e1e0146
pengyi14 <pengyi14@wanda.cn>
2017-08-16 17:06:46 +0800

    dsa

---
+++ RepluginDemo/app/src/main/java/com/qihoo360/replugin/sample/util/Test.java
@@ -0,0 +1,12 @@
+package com.qihoo360.replugin.sample.util;
+
+/**
+ * Created by pengyi on 2017/8/16.
+ */
+
+public class Test {
+
+    public static  void main(String [] args) {
+        System.out.print("hello world");
+    }
+}
